b3504e5945372b6ec444204930b61881c30fe898
svasenkov <aanher@gmail.com>
2023-12-18 19:50:53 +0100

    added remote driver

--- src/test/java/MainPageTests.java
+++ src/test/java/MainPageTests.java
@@ -1,6 +1,3 @@
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,12 +6,7 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 
-public class MainPageTests {
-
-    @BeforeAll
-    static void setup() {
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-    }
+public class MainPageTests extends TestBase {
 
     @Test
     @DisplayName("Check label \"Let’s build from here\" on main page")
---
+++ src/test/java/TestBase.java
@@ -0,0 +1,14 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+
+    @BeforeAll
+    static void setup() {
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+}
