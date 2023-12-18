5769bca797c36c29ad9bdaae9725856fd6b13dd6
svasenkov <aanher@gmail.com>
2023-12-18 19:27:16 +0100

    added first tests

---
+++ src/test/java/SearchTests.java
@@ -0,0 +1,37 @@
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+
+public class SearchTests {
+
+    @BeforeAll
+    static void setup() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+
+    @Test
+    @DisplayName("Check label successful search")
+    void successfulSearchTest() {
+        step("Open main page", ()->
+                open("https://github.com"));
+        step("Type search", ()-> {
+            $(".header-search-button").click();
+            $("#query-builder-test").setValue("qa.guru").pressEnter();
+        });
+        step("Check search results", ()->
+                $("[data-testid=\"results-list\"]").shouldHave(text("qa-guru/niffler")));
+    }
+
+    @Test
+    void unsuccessfulSearchTest() {
+
+    }
+}
